c938ad59d53fee39978366b8af53bd8e8ea31bdc
Arupkumar7978 <arupkumarpadhi68@gmail.com>
2023-05-14 14:53:10 +0530

    [EC-02] | Added Components , Config and additional files

---
+++ src/Components/Cards/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cards = () => {
+  return <>Cards Component</>;
+};
+
+export default Cards;
---
+++ src/Components/UI Window/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ApplicationLayer() {
+  return (
+    <>
+      <h1> ALL THE COMPONENTS WILL BE RENDERED FROM HERE</h1>
+    </>
+  );
+}
