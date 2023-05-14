28f28e2a1572dae1555ca7456312d93431efe0d2
Arupkumar7978 <arupkumarpadhi68@gmail.com>
2023-05-14 15:09:28 +0530

    Added Center Tag

--- src/Components/UI Window/index.jsx
+++ src/Components/UI Window/index.jsx
@@ -4,7 +4,7 @@ import AdditionalConfig from '../../Configurations/AdditionalConfig';
 export default function ApplicationLayer() {
   return (
     <>
-      <h1> ALL THE COMPONENTS WILL BE RENDERED FROM HERE</h1>
+      <center> ALL THE COMPONENTS WILL BE RENDERED FROM HERE</center>
     </>
   );
 }
